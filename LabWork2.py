a27ceaaf3f2522c81fb1c99e1696b69babd07853
VegaMJ28 <mjusa28@gmail.com>
2026-09-25 09:36:44 -0500

    Add files via upload

--- LabWork2.py
+++ LabWork2.py
@@ -2,25 +2,37 @@ myEmployees = {}
 i = 1
 
 def add(i):
-    name =
-    basic_pay =
-    allowance =
-    deductions =
-    taxes =
-    gross_pay =
-    net_pay =
+    name = input("Enter employee name: ")
+    basic_pay = int(input("Enter basic pay: "))
+    allowance = int(input("Enter allowance: "))
+    deductions = int(input("Enter deductions: "))
+    taxes = int(input("Enter taxes: "))
+    gross_pay = basic_pay + allowance
+    net_pay = gross_pay - deductions - taxes
 
-    myEmployees.update({"employee"+str(i): {"name": }})
+    myEmployees.update({"employee"+str(i): {"name": name, "basic pay": basic_pay, "allowance": allowance, "deductions": deductions, "taxes": taxes,"gross pay": gross_pay, "net pay": net_pay }})
 
 def delete():
-    delete_employee =
+    delete_employee = input("Enter employee key to be deleted: ")
+    if delete_employee in myEmployees:
+        del myEmployees[delete_employee]
+
+    else:
+        print("Employee not found")
 
 def modify():
-    modify_employee =
+    modify_employee = input("Enter employee key to be modified: ")
 
     if modify_employee in myEmployees:
-        myEmployees[modify_employee]["basic pay"]=
+        myEmployees[modify_employee]["basic pay"]= int(input("Enter basic pay: "))
+        myEmployees[modify_employee]["allowance"] = int(input("Enter allowance: "))
+        myEmployees[modify_employee]["deductions"] = int(input("Enter deductions: "))
+        myEmployees[modify_employee]["taxes"] = int(input("Enter taxes: "))
+        myEmployees[modify_employee]["gross pay"] = myEmployees[modify_employee]["basic pay"] + myEmployees[modify_employee]["allowance"]
+        myEmployees[modify_employee]["net pay"] = myEmployees[modify_employee]["gross pay"] - myEmployees[modify_employee]["deductions"] - myEmployees[modify_employee]["taxes"]
 
+    else:
+        print("Employee not found")
 
 def display():
     if len(myEmployees) ==0:
@@ -30,4 +42,38 @@ def display():
         for employee in myEmployees:
             print(employee)
             print("name:", myEmployees[employee]["name"])
+            print("basic pay:", myEmployees[employee]["basic pay"])
+            print("allowance:", myEmployees[employee]["allowance"])
+            print("deductions:", myEmployees[employee]["deductions"])
+            print("taxes:", myEmployees[employee]["taxes"])
+            print("gross pay:", myEmployees[employee]["gross pay"])
+            print("net pay:", myEmployees[employee]["net pay"])
 
+while 1:
+    print("Employee Payroll Management Dictionary")
+    print("1. Add employee")
+    print("2. Delete employee")
+    print("3. Modify employee")
+    print("4. Display")
+    print("5. Exit")
+    choice = int(input("Enter your choice: "))
+
+    if choice == 5:
+        print("exiting...")
+        break
+
+    elif choice == 1:
+        add(i)
+        i += 1
+
+    elif choice == 2:
+        delete()
+
+    elif choice == 3:
+        modify()
+
+    elif choice == 4:
+        display()
+
+    else:
+        print("Invalid choice")
